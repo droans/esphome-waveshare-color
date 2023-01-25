1d33b6f483963515dd490efbe78270a660b36fb0
Michael Carroll <mjoecarroll@gmail.com>
2023-01-25 07:32:29 -0500

    Add Booster settings for 7P5InV2

--- esphome/components/waveshare_epaper/waveshare_epaper.cpp
+++ esphome/components/waveshare_epaper/waveshare_epaper.cpp
@@ -933,6 +933,13 @@ void WaveshareEPaper7P5InV2::initialize() {
   this->data(0x3f);
   this->command(0x04);
 
+  // BOOSTER SETTING
+  this->command(0x06);
+  this->data(0x17);
+  this->data(0x17);
+  this->data(0x24);
+  this->data(0x17);
+  
   delay(100);  // NOLINT
   this->wait_until_idle_();
   // COMMAND PANEL SETTING
